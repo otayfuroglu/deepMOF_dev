4e32dd610b1dd361b902d22d04e99f158d0fb7eb
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-01-11 10:42:27 +0300

    read data base path from run sh file

--- works/runTest/runValCalculation.sh
+++ works/runTest/runValCalculation.sh
@@ -7,7 +7,9 @@ PYDIR=$HOME/miniconda3/envs/python38/bin
 MODEL_DIR="schnet_l3_basis96_filter64_interact3_gaussian20_rho001_lr00001_bs1_cutoff_60_withoutStress_aseEnv_IRMOFseries1_4_6_7_10_merged_173014_ev"
 
 filesDIR=$SCRATCH/deepMOF/HDNNP/prepare_data/geomFiles/MOF5/broken_bond
-for val_type in fromFiles #aliphatic_ch_bond # aromatic_ch_bond torsion
+db_path=$SCRATCH/deepMOF_dev/data_bases/breken_bond_irmofseries1_f1_ev_testData.db
+
+for val_type in fromDB
 do
 	echo $val_type
 	RESULT_DIR=$SCRATCH/deepMOF_dev/works/runTest/broken_bond
@@ -15,7 +17,9 @@ do
 	mkdir -p $RESULT_DIR
 
 	$PYDIR/python $SCRIPTDIR/calcSPwithSchnetpack.py \
-		-val_type $val_type -MODEL_DIR $MODEL_DIR -RESULT_DIR $RESULT_DIR -filesDIR $filesDIR
+		-val_type $val_type -MODEL_DIR $MODEL_DIR \
+		-RESULT_DIR $RESULT_DIR -filesDIR $filesDIR \
+		-db_path $db_path
 done
 echo All done
 
