a1792e97bd20a1f0c4644f3caa7daf19f7e3ae2a
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-01-12 13:42:50 +0300

    running file QMMM calculations

---
+++ prediction/runQMMMCalculationsWithAse.py
@@ -0,0 +1,206 @@
+#! /truba/home/yzorlu/miniconda3/bin/python -u
+#
+from calculationsWithAse import AseCalculations
+from schnetpack.environment import AseEnvironmentProvider
+
+#  from schnetpack import Properties
+from schnetpack.interfaces import SpkCalculator
+from schnetpack.utils import load_model
+#  from schnetpack.datasets import AtomsData
+import schnetpack
+
+#  from ase import units
+from ase.optimize import BFGS, LBFGS, GPMin, QuasiNewton
+from ase.calculators.calculator import Calculator, all_changes
+from ase.io.trajectory import Trajectory
+#  from ase.io.xyz import read_xyz,  write_xyz
+#  from ase.io import read, write
+
+import numpy as np
+import torch
+import os
+import shutil
+#  import tqdm
+#  from writeTestOutput import writeTestOutput
+
+import getpass
+USER = getpass.getuser()
+
+properties = ["energy", "forces", "stress"]  # properties used for training
+BASE_DIR = f"/truba_scratch/{USER}/deepMOF/HDNNP/"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# in case multiprocesses, global device variable rise CUDA spawn error.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpus = torch.cuda.device_count()
+print("Number of cuda devices --> %s" % n_gpus,)
+
+
+def getWorksDir(calc_name):
+
+    WORKS_DIR = calc_name
+    if os.path.exists(WORKS_DIR):
+        shutil.rmtree(WORKS_DIR)
+    os.mkdir(WORKS_DIR)
+
+    return WORKS_DIR
+
+
+def getMLcalculator():
+
+    MODEL_DIR = BASE_DIR \
+            + "/schnetpack" \
+            + "/runTraining" \
+            + "/schnet_l3_basis96_filter64_interact3_gaussian20_rho001_lr00001_bs1_cutoff_60_"\
+            + "withoutStress_aseEnv_test_IRMOFseries1_4_6_7_10_merged_173014_ev"
+    #  + "/hdnnBehler_l3n100_rho001_r20a5_lr0001_bs1_IRMOFseries%s_merged_72886_ev" %mof_num\
+
+    model_path = os.path.join(MODEL_DIR, "best_model")
+    model = load_model(model_path, map_location=device)
+    if "stress" in properties:
+        print("Stress calculations are active")
+        schnetpack.utils.activate_stress_computation(model)
+    try:
+        stress = properties[2]
+    except:
+        stress = None
+    calculator = SpkCalculator(
+        model,
+        device=device,
+        collect_triples=False,
+        environment_provider=AseEnvironmentProvider(cutoff=5.5),
+        energy=properties[0],
+        forces=properties[1],
+        stress=stress,
+        energy_units="eV",
+        forces_units="eV/Angstrom",
+        stress_units="eV/Angstrom/Angstrom/Angstrom",
+        )
+
+    return calculator
+
+
+def getLJcalculator():
+    from ase.calculators.lj import LennardJones
+
+    calculator = LennardJones()
+    calculator.parameters.epsilon = 0.0102996
+    calculator.parameters.sigma = 3.4
+    calculator.parameters.rc = 12.0
+
+    return calculator
+
+
+def run(file_base, molecule_path, calc_type, run_type):
+
+    temp = 300
+    asePlatformDIR = BASE_DIR + "/schnetpack/asePlatform"
+    name = file_base + "_%s_%s_%sK" % (calc_type, run_type, temp)
+    CW_DIR = os.getcwd()
+
+    # main directory for caculation runOpt
+    if not os.path.exists("run_worksdir"):
+        os.mkdir("run_worksdir")
+
+    WORKS_DIR = getWorksDir(asePlatformDIR + "/run_worksdir/" + name)
+
+    os.chdir(WORKS_DIR)
+
+    calculation = AseCalculations(WORKS_DIR)
+    calculation.setCalcName(name)
+
+    calculation.load_molecule_fromFile(molecule_path)
+    P = [[0, 0, -1], [0, -1, 0], [-1, 0, 0]]
+    calculation.makeSupercell(P)
+
+
+    calculation.setQMMMForceCalculator(
+        qm_musk=range(calculation.molecule.get_number_of_atoms()),
+        qm_calcultor=getMLcalculator(),
+        mm_calcultor=getLJcalculator(),
+        buffer_width=12.0
+    )
+
+    if run_type == "opt":
+        calculation.optimize()
+        os.chdir(CW_DIR)
+
+    elif run_type == "vibration":
+        #  WORKS_DIR = WORKS_DIR.replace("vibration", "opt").replace("DFT", "model")
+        #  opt_molpath= "%s/Optimization.xyz" %WORKS_DIR
+        #  calculation.load_molecule_fromFile(molecule_path=opt_molpath)
+        calculation.optimize(fmax=0.015)
+        calculation.vibration(nfree=4)
+        os.chdir(CW_DIR)
+
+    elif run_type == "md":
+
+        calculation.init_md(
+          name=name,
+          time_step=0.5,
+          temp_init=100.0,
+          # temp_bath should be None for NVE and NPT
+          temp_bath=temp,
+          # temperature_K for NPT
+          #  temperature_K=temp,
+          interval=5,
+        )
+
+        calculation.optimize(fmax=0.02)
+        calculation.run_md(1000000)
+
+        #  setting strain for pressure deformation simultaions
+
+        #  lattice direction a
+        #  abc = calculation.molecule.cell.lengths()
+        #  a = abc[0]
+
+        #  for i in range(149):
+        #      print("\nStep %s\n" %i)
+        #      a -= 0.003 * a
+        #      abc[0] = a
+        #      calculation.molecule.set_cell(abc)
+        #      calculation.run_md(5000)
+
+    elif run_type == "EOS":
+
+        cell = calculation.molecule.get_cell()
+
+
+        traj_name = "%s_%s_EOS.traj" % (file_base, calc_type)
+        traj = Trajectory(traj_name, "w")
+        scaleFs = np.linspace(0.98, 1.10, 12)
+        print(len(scaleFs))
+        for scaleF in scaleFs:
+            calculation.molecule.set_cell(cell * scaleF, scale_atoms=True)
+            calculation.get_potential_energy()
+
+            traj.write(calculation.molecule)
+        os.chdir(CW_DIR)
+
+
+    elif run_type == "optLattice":
+        name = file_base + "_calc_lattice_const"
+        from ase.constraints import StrainFilter, UnitCellFilter
+
+        #  calculation.setCalcName("mof5Lattice")
+        #  calculation.molecule.set_pbc([True, True, True])
+        print(calculation.molecule.get_cell_lengths_and_angles())
+        calculation.optimizeWithStrain(fmax=0.0005)
+        print(calculation.molecule.get_cell_lengths_and_angles())
+
+        calculation.optimize(fmax=0.05)
+
+
+def main():
+    mof_num = 1
+    file_base = "mercury_IRMOF%s" %mof_num
+
+    MOL_DIR = BASE_DIR + "/prepare_data/geomFiles/IRMOFSeries/cif_files"
+    molecule_path = os.path.join(MOL_DIR, "%s.cif" %file_base)
+
+    calc_type = "model"
+    run_type = "optLattice"
+
+    run(file_base, molecule_path, calc_type, run_type)
+
+main()
