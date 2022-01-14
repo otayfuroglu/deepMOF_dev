0bd001890ba38d788f25e99d04fbfcbfdb5bd101
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-01-14 15:55:58 +0300

    black box

--- utils/packingCifFilewithObabel.py
+++ utils/packingCifFilewithObabel.py
@@ -1,30 +1,37 @@
-import openbabel
+from openbabel import openbabel
+import os
+import argparse
 
-def cif_to_xyz_babel(xyz_path, cif_path):
+
+def cif_to_cif_babel(cif_path, out_format):
 
     obConversion = openbabel.OBConversion()
-    obConversion.SetInAndOutFormats("cif", "xyz")
+    obConversion.SetInAndOutFormats("cif", out_format)
     myOBMol = openbabel.OBMol()
     obConversion.ReadFile(myOBMol, cif_path)
     fillUC = openbabel.OBOp.FindType("fillUC")
     fillUC.Do(myOBMol, "strict")
-    obConversion.WriteFile(myOBMol, xyz_path)
 
+    split_path = cif_path.split("/")
+    file_name = split_path[-1].replace("cif", out_format)
+    new_file_name = "filled_" + file_name
 
-def cif_to_cif_babel(new_cif_path, cif_path):
-
-    obConversion = openbabel.OBConversion()
-    obConversion.SetInAndOutFormats("cif", "cif")
-    myOBMol = openbabel.OBMol()
-    obConversion.ReadFile(myOBMol, cif_path)
-    fillUC = openbabel.OBOp.FindType("fillUC")
-    fillUC.Do(myOBMol, "strict")
+    if len(split_path) == 1:
+        new_cif_path = new_file_name
+    else:
+        new_cif_path = "/".join(split_path[:-1]) + "/" + new_file_name
     obConversion.WriteFile(myOBMol, new_cif_path)
 
 
-xyz_path = "deneme.xyz"
-cif_path = "./IRMOF-10.cif"
-new_cif_path = "./obabel_IRMOF-10.cif"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Give samefile.hdf5 file name")
+    parser.add_argument(
+        "-cifPath", "--cifPath", type=str,
+        required=True, help="give hdf5 file base")
+    parser.add_argument(
+        "-outFormat", "--outFormat", type=str,
+        required=True, help="give hdf5 file base")
 
-#  cif_to_xyz_babel(xyz_path, cif_path)
-cif_to_cif_babel(new_cif_path, cif_path)
+    args = parser.parse_args()
+    cif_to_cif_babel(args.cifPath, args.outFormat)
