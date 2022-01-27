8ad6e90141cb0777620c14cf322c2b4d6aa3733e
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-01-27 16:13:55 +0300

    arrangement some prop

--- works/runMD/runMDWithSchnetpack.sh
+++ works/runMD/runMDWithSchnetpack.sh
@@ -5,23 +5,23 @@ NSAMPLE=1000000
 MDTYPE="md"
 MODE="md"
 # MODE="ensamble"
-mof_num=10
-descrip_word="_NPT_111cell_allNNP_without5"
+mof_num=1
+descrip_word="_deneyLJ"
 # len_data=97355
 
-BASEDIR="/truba_scratch/yzorlu/deepMOF/HDNNP/prepare_data"
-PYTHON38DIR="/truba/home/yzorlu/miniconda3/bin/"
-SCRIPTDIR="/truba_scratch/yzorlu/deepMOF/HDNNP/schnetpack"
+BASEDIR="/truba_scratch/$USER/deepMOF_dev"
+PYTHON38DIR="$HOME/miniconda3/envs/python38/bin/"
+SCRIPTDIR="$BASEDIR/md"
 
 
-molName=obabel_IRMOF${mof_num}
+molName=IRMOF${mof_num}
 echo $molName
-MOLPATH=$BASEDIR/geomFiles/IRMOFSeries/cif_files/$molName.cif
+MOLPATH=$BASEDIR/geom_files/IRMOFSeries/cif_files/$molName.cif
 # MODEL1_DIR="hdnnBehler_l3n50_rho001_r20a5_lr0001_bs1_IRMOFseries${mof_num}_merged_${len_data}_ev"
-MODEL1_DIR="schnet_l3_basis96_filter64_interact3_gaussian20_rho001_lr00001_bs1_cutoff_60_withoutStress_aseEnv_test_IRMOFseries1_4_6_7_10_merged_173014_ev"
+MODEL1_DIR=$BASEDIR/works/runTrain/schnet_l3_basis96_filter64_interact3_gaussian20_rho001_lr00001_bs1_cutoff_60_withoutStress_aseEnv_test_IRMOFseries1_4_6_7_10_merged_173014_ev
 # MODEL2_DIR="hdnnBehler_l3n50_rho001_r20a5_lr0001_bs1_IRMOFseries${mof_num}_merged_${len_data}_ev"
 
-$PYTHON38DIR/python -u $SCRIPTDIR/run_md_withSchnetpack.py \
+$PYTHON38DIR/python -u $SCRIPTDIR/runMDwithSchnetpack.py \
 	-mol $MOLPATH -mdtype $MDTYPE\
 	-n $NSAMPLE -temp 100\
 	-init_temp 100\
