a5a47de3286195db74cbb1902ee05d50d488ec3e
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-02-04 16:32:28 +0300

    small fix

--- n2p2/train/aseDB2n2p2.py
+++ n2p2/train/aseDB2n2p2.py
@@ -6,16 +6,20 @@ db = connect(db_path).select()
 
 fl = open("input.data", "w")
 for row in db:
-    atoms_prep_list = [["begin"], ["comment\t", row.name]]
-    atoms_prep_list += [["atom"] + coord + [sym] + [0.0, 0.0] + forces
-                        for coord, sym, forces in zip(row.positions.tolist(),
-                                                      row.symbols,
-                                                      row.forces.tolist())]
-    atoms_prep_list += [["energy", row.energy], ["charge 0.0"], ["end"]]
+    atoms_prep_list = [["begin"], ["comment ", row.name]]
+
+    atom_template = 'atom {:10.6f} {:10.6f} {:10.6f} {:2s} {:10.6f} {:10.6f} {:10.6f} {:10.6f} {:10.6f}'
+    atoms_prep_list += [[atom_template.format(
+        position[0], position[1], position[2],
+        symbol, 0.0, 0.0,
+        forces[0], forces[1], forces[2])]
+        for position, symbol, forces in zip(
+            row.positions.tolist(), row.symbols, row.forces.tolist())]
+
+    atoms_prep_list += [["energy ", row.energy], ["charge 0.0"], ["end"]]
     for line in atoms_prep_list:
         for item in line:
             fl.write(str(item))
-            fl.write("\t")
         fl.write("\n")
 fl.close()
 
