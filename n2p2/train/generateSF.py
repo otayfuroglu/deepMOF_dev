d5c2e04d09c26eaddccff7805e3b8ae6f576b88a
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-02-16 19:16:09 +0300

    small

--- n2p2/train/generateSF.py
+++ n2p2/train/generateSF.py
@@ -13,28 +13,28 @@ myGenerator = SymFuncParamGenerator( elements=['H', 'C', 'O', 'Zn'],
                                     r_cutoff = 6.0)
 
 
-myGenerator.symfunc_type = 'radial'
-myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='shift', nb_param_pairs=6)
+myGenerator.symfunc_type = 'weighted_radial'
+myGenerator.generate_radial_params(rule='gastegger2018',
+                                   mode='shift', nb_param_pairs=15, r_lower=0.7)
 
 f = open('sym_func_params.txt', 'w')
 myGenerator.write_settings_overview(fileobj=f)
 myGenerator.write_parameter_strings(fileobj=f)
 
-zetas = [1, 2]
-myGenerator.symfunc_type = 'angular_narrow'
+zetas = [1, 6]
+myGenerator.symfunc_type = 'weighted_angular'
 myGenerator.zetas = zetas
-myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='center', nb_param_pairs=3, r_lower=1.5)
+myGenerator.generate_radial_params(rule='gastegger2018',
+                                   mode='center', nb_param_pairs=3, r_lower=0.7)
 myGenerator.write_settings_overview(fileobj=f)
 myGenerator.write_parameter_strings(fileobj=f)
 
-myGenerator.symfunc_type = 'angular_wide'
-myGenerator.zetas = zetas
-myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='center', nb_param_pairs=3, r_lower=1.5)
-myGenerator.write_settings_overview(fileobj=f)
-myGenerator.write_parameter_strings(fileobj=f)
+#  myGenerator.symfunc_type = 'angular_wide'
+#  myGenerator.zetas = zetas
+#  myGenerator.generate_radial_params(rule='imbalzano2018',
+#                                     mode='center', nb_param_pairs=3, r_lower=1.5)
+#  myGenerator.write_settings_overview(fileobj=f)
+#  myGenerator.write_parameter_strings(fileobj=f)
 
 
 
