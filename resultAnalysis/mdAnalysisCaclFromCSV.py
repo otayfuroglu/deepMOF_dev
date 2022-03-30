3e5195ae4c73efe2139151c6af4112c70c32a038
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-03-30 20:12:37 +0300

    clearification after copy

--- resultAnalysis/mdAnalysisCaclFromCSV.py
+++ resultAnalysis/mdAnalysisCaclFromCSV.py
@@ -16,10 +16,10 @@ def calcVEC():
 
     mean_ln_volumes = []
     mean_length = []
-    job_names = ["mercury_IRMOF%s_%dK_%s_NPT_111cell_allNNP_without5" % (mof_num, temp, md_type) for temp in temp_list]
+    job_names = ["%s_%dK_%s_ani" %(file_base, temp, md_type) for temp in temp_list]
     for job_name in job_names:
         #  print(job_name)
-        csv_file_path = "%s/timeEnergyTempVolume_%s.csv" % (RESULTS_DIR, job_name)
+        csv_file_path = "%s/%s/timeEnergyTempVolume_%s.csv" % (MD_DIR, job_name, job_name)
 
         df = pd.read_csv(csv_file_path, skiprows=range(1, initial_skip)) # read colum names and skip
 
@@ -37,7 +37,7 @@ def calcVEC():
     df[""] = mean_length
     df["MeanLength"] = mean_length
     df["LnMeanVolume"] = mean_ln_volumes
-    df.to_csv("%s/temperatureLengthLnVolume_%s.csv" %(RESULTS_DIR, mof_name))
+    df.to_csv("%s/temperatureLengthLnVolume_%s.csv" %(RESULTS_DIR, file_base))
 
     return(m, b)
 
@@ -59,49 +59,55 @@ def calcBulkMod(df):
 
 #  calcVEC()
 
-BASE_DIR = "/truba_scratch/yzorlu/deepMOF/HDNNP/schnetpack"
-initial_skip = int(3e5)
-len_totaldata = 1e6
+BASE_DIR = "/truba_scratch/otayfuroglu/deepMOF_dev"
+initial_skip = int(3e4)
+len_totaldata = 1e5
 end_row_idx = int(len_totaldata - initial_skip)
 properties = ["energy", "forces"]
 
-sub_jobname = "tec_md"
+sub_jobname = ""
 md_type = "md"
-RESULTS_DIR = "%s/mdAnalysis/results/%s" % (BASE_DIR, sub_jobname)
+MD_DIR = BASE_DIR + "/ani/works/runMD/" + sub_jobname
+RESULTS_DIR = BASE_DIR + "/ani/works/runMD/results"
 
 
-temp_list = np.arange(200, 401, 50)
+temp_list = np.arange(200, 451, 50)
 mof_tec = np.empty((0, 3))
-for mof_num in [1, 4, 6, 7, 10]:
-    mof_name = "IRMOF%s" %mof_num
-    print(mof_name)
+
+file_bases = ["filled_09000N3"]
+for file_base in file_bases:
+    print(file_base)
 
     m, b = calcVEC()
-    mof_tec = np.append(mof_tec, np.array([[mof_name, m, b]]), axis=0)
+    mof_tec = np.append(mof_tec, np.array([[file_base, m, b]]), axis=0)
 
 # save to csv
 df_mof_tec = pd.DataFrame()
-df_mof_tec["IRMOF%d"%mof_num] = mof_tec[:, 0]
+df_mof_tec["FileName"] = mof_tec[:, 0]
 df_mof_tec["Slope"] = mof_tec[:, 1]
 df_mof_tec["Intercept"] = mof_tec[:, 2]
 df_mof_tec.to_csv("%s/allMofTEC.csv" %RESULTS_DIR)
 
 
-#  for mof_num in [1, 4, 6, 7, 10]:
-#      mof_name = "IRMOF%s" %mof_num
-#      print(mof_name)
-#
-#      temp = 300
-#      job_name = "mercury_IRMOF%s_%dK_%s_NPT_111cell_allNNP_without5" % (mof_num, temp, md_type)
-#      RESULTS_DIR = "%s/mdAnalysis/results/%s" % (BASE_DIR, sub_jobname)
-#      csv_file_path = "%s/timeEnergyTempVolume_%s.csv" % (RESULTS_DIR, job_name)
-#      df = pd.read_csv(csv_file_path, skiprows=range(1, initial_skip)) # read colum names and skip
-#
-#      # set data range from skip initial to end of row according to desired total lenth of data
-#      df = df[:end_row_idx]
-#      calcBulkMod(df)
-#
-#      print("*" * 20)
+file_bases = ["filled_09000N3"]
+for file_base in file_bases:
+    print(file_base)
+
+    temp = 300
+    md_type = "md"
+    BASE_DIR = "/truba_scratch/otayfuroglu/deepMOF_dev"
+    job_name = "%s_%dK_%s_ani" %(file_base, temp, md_type)
+    resut_file_name = "%s_%s.hdf5"  %(file_base, md_type)
+    sub_jobname = ""
+    MD_DIR = BASE_DIR + "/ani/works/runMD/" + sub_jobname + "/" + job_name
+    csv_file_path = "%s/timeEnergyTempVolume_%s.csv" % (MD_DIR, job_name)
+    df = pd.read_csv(csv_file_path, skiprows=range(1, initial_skip)) # read colum names and skip
+
+    # set data range from skip initial to end of row according to desired total lenth of data
+    df = df[:end_row_idx]
+    calcBulkMod(df)
+
+    print("*" * 20)
 
 
 
