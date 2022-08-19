6303911fdf498214f2aaa4b36e15f32555dbff2f
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2022-08-19 18:56:17 +0300

    added prallel processing

--- resultAnalysis/mdAnalysisLammpsTraj.py
+++ resultAnalysis/mdAnalysisLammpsTraj.py
@@ -1,4 +1,5 @@
 #
+from multiprocessing import Pool
 from ase.io import read
 import numpy as np
 import pandas as pd
@@ -29,6 +30,33 @@ def create_pd_row(label_val):
         df[label] = [val]
     return df
 
+
+def run(traj_file):
+    print("Processing", traj_file)
+    # get pressure from file name
+    press =int(traj_file.lower()[traj_file.lower().index(gas_type):traj_file.lower().index("bar")]
+               .replace("_", "").replace(gas_type.lower(), ""))
+
+    traj = read("%s/%s" %(traj_dir, traj_file), format="lammps-dump-text", index=":")
+    f_start = args.skip
+    f_end = len(traj)
+    n_frame = f_end - f_start
+    fram_mass = calcFramMass(traj[0], **atom_mass)
+    gas_loads = np.zeros(n_frame)
+    for i, atoms in enumerate(traj[f_start:f_end]):
+        gas_loads[i] = calcGasNum(atoms, gas_id)
+    avg_gas_loads = gas_loads.mean()
+    avg_gas_masses = avg_gas_loads * gas_mass
+
+    print("Pressure: ", press)
+    print(f"Number of loaded gas molecule (avg.): {avg_gas_loads}")
+    print(f"Weight of loaded gas molecule (avg.): {avg_gas_masses}")
+    print(f"wt%: {(avg_gas_masses / fram_mass) * 100}")
+
+    #return as a df
+    return create_pd_row({"Pressure": press, "AvgGasLoads": avg_gas_loads,
+                              "AvgGasMasses": avg_gas_masses,
+                              "AvgGasMasses(wt%)": (avg_gas_masses / fram_mass) * 100})
 
 if __name__ == "__main__":
 
@@ -74,36 +102,11 @@ if __name__ == "__main__":
     #  trajfile = "/truba_scratch/yzorlu/deepMOF_dev/n2p2/works/runMD/flexAdorpsionH2onIRMOF1inNPT/IRMOF1_H2_100Bar_77.0K.lammpstrj"
     #  trajfile = "/truba_scratch/yzorlu/deepMOF_dev/n2p2/works/runMD/classicFlexAdorpsionH2onIRMOF1inNPT/IRMOF1_H2_100Bar_77K.lammpstrj"
 
-    dfs = []
     traj_files = [traj_file for traj_file in os.listdir(traj_dir) if ".lammpstrj" in traj_file]
-    for traj_file in traj_files:
-        print("Processing", traj_file)
-        traj = read("%s/%s" %(traj_dir, traj_file), format="lammps-dump-text", index=":")
-        f_start = args.skip
-        f_end = len(traj)
-
-        # get pressure from file name
-        press =int(traj_file[traj_file.index(gas_type.upper()):traj_file.index("Bar")]
-                   .replace("_", "").replace(gas_type.upper(), ""))
-        print(press)
-
-        n_frame = f_end - f_start
-        fram_mass = calcFramMass(traj[0], **atom_mass)
-        gas_loads = np.zeros(n_frame)
-
-        for i, atoms in enumerate(traj[f_start:f_end]):
-            gas_loads[i] = calcGasNum(atoms, gas_id)
-        avg_gas_loads = gas_loads.mean()
-        avg_gas_masses = avg_gas_loads * gas_mass
-
-        dfs.append(create_pd_row({"Pressure": press, "AvgGasLoads": avg_gas_loads,
-                                  "AvgGasMasses": avg_gas_masses,
-                                  "AvgGasMasses(%wt)": (avg_gas_masses / fram_mass) * 100}))
-
-        print(f"Number of loaded gas molecule (avg.): {avg_gas_loads}")
-        print(f"Weight of loaded gas molecule (avg.): {avg_gas_masses}")
-        print(f"%wt: {(avg_gas_masses / fram_mass) * 100}")
-
+    print("Created process up to number of traj_files")
+    with Pool(len(traj_files)) as pool:
+        dfs = pool.map(run, traj_files)
 
     df = pd.concat(dfs).reset_index()
     df.to_csv(f"loading_{gas_type}.csv")
+
