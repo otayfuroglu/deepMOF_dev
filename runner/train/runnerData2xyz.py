eb225f0976de015e4ff04f87b4ef449b01917e6d
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2023-01-06 21:28:15 +0300

    a single runner data file corver to .xyz format

---
+++ runner/train/runnerData2xyz.py
@@ -0,0 +1,13 @@
+
+file_name = "input.data"
+with open(f"{file_name.split('.')[0]}.xyz", "w") as fl:
+    lines = open(file_name).readlines()
+    fl.write(f"{len(lines)-5}\n")
+    fl.write("from runner data\n")
+    for line in lines[2:-3]:
+        print(line)
+        if line in ["begin", "energy", "charge", "end"]:
+            continue
+        elements = line.split()
+        fl.write(f"{elements[4]} {' '.join(elements[1:4])} {' '.join(elements[5:])}\n")
+
