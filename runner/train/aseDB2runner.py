226968c4e9d93c3fd1a8aeb06b0b7e033cf45e88
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-02-24 19:12:46 +0100

    new version of aseDB2runner

--- runner/train/aseDB2runner.py
+++ runner/train/aseDB2runner.py
@@ -1,100 +1,93 @@
-from ase.db import connect
+
 from ase import units
-import pandas as pd
+from ase.db import connect
+import numpy as np
+
 import tqdm
-import random
-from multiprocessing import Pool
+import argparse
+import os
+#  import shutil
 
 
 u = units.create_units("2014")
 EV2HARTREE = 1.0 / u["Hartree"]
 ANG2BOHR = 1.0 / u["Bohr"]
 
-#  free_atoms_energies = {
-#      "H":-0.498629869288485,
-#      "C":-37.75219657637893,
-#      "O":-74.92421247265553,
-#      "Zn":-1778.8529650280605,
-#  }
+def aseDb2Runner(db):
+    db = db.select()
+    fl = open(f"input.data", "w")
+    for i, row in enumerate(tqdm.tqdm(db)):
+        # remove random value for efficiency
+        atoms_prep_list = [["begin"]]
+        atoms_prep_list += [["lattice    {:15.8f}    {:15.8f}    {:15.8f}".format(
+                cell[0], cell[1], cell[2])] for cell in row.cell * ANG2BOHR]
 
-free_atoms_energies = {
-    "H": -13.56841632652680,
-    "C": -1027.290084166281,
-    "O": -2038.792640354206,
-    "Zn": -48405.0777937116,  # Free atom reference energy in eV
-}
+        atom_template = 'atom {:15.8f} {:15.8f} {:15.8f} {:2s} {:15.8f} {:15.8f} {:15.8f} {:15.8f} {:15.8f}'
+        atoms_prep_list += [[atom_template.format(
+            position[0], position[1], position[2],
+            symbol, charge, 0.0,
+            forces[0], forces[1], forces[2])]
+            for position, symbol, charge, forces in zip(
+                (row.positions * ANG2BOHR).tolist(),
+                row.symbols,
+                row.data.charges,
+                #  chgarges_list,
+                (np.array(row.data.forces) * (EV2HARTREE/ANG2BOHR)).tolist())]
 
-def getWriteRowInfo(idx):
-    row = db.get(idx)
-    if conf_type:
-        if conf_type not in row.name:
-            return 0
-    atoms_prep_list = [["begin"], ["comment ", row.name]]
-    atom_template = 'atom {:10.6f} {:10.6f} {:10.6f} {:2s} {:10.6f} {:10.6f} {:10.6f} {:10.6f} {:10.6f}'
-    atoms_prep_list += [[atom_template.format(
-        position[0], position[1], position[2],
-        symbol, 0.0, 0.0,
-        forces[0], forces[1], forces[2])]
-        for position, symbol, forces in zip(
-            (row.positions * ANG2BOHR).tolist(), row.symbols,
-            (row.forces * (EV2HARTREE/ANG2BOHR)).tolist())]
-            #  row.positions.tolist(), row.symbols, row.forces.tolist())]
-    atoms_prep_list += [["energy ",
-                         row.energy * EV2HARTREE], ["charge 0.0"], ["end"]]
+        atoms_prep_list += [["energy ",
+                             row.data.energy * EV2HARTREE], ["charge 0.0"], ["end"]]
 
-    #  return atoms_prep_list, row.energy * EV2HARTREE / row.natoms
-    return atoms_prep_list, calc_cohesive_E(row, row.energy, free_atoms_energies)
+        for line in atoms_prep_list:
+            for item in line:
+                fl.write(str(item))
+            fl.write("\n")
+    fl.close()
 
+def rndAseDb2Runner(db):
+    import random
+    rand_list = random.sample(range(db.count()), N)
+    db = db.select()
+    fl = open(f"input.data.rand{N}", "w")
+    for i, row in enumerate(tqdm.tqdm(db)):
+        if i in rand_list:
+            # remove random value for efficiency
+            rand_list.remove(i)
+            atoms_prep_list = [["begin"]]
+            atoms_prep_list += [["lattice    {:15.8f}    {:15.8f}    {:15.8f}".format(
+                    cell[0], cell[1], cell[2])] for cell in row.cell * ANG2BOHR]
 
-def run_func():
-    for result in tqdm.tqdm(
-        pool.imap_unordered(
-            func=getWriteRowInfo, iterable=iterable), total=n_db):
-        if result != 0:
-            for line in result[0]:
+            atom_template = 'atom {:15.8f} {:15.8f} {:15.8f} {:2s} {:15.8f} {:15.8f} {:15.8f} {:15.8f} {:15.8f}'
+            atoms_prep_list += [[atom_template.format(
+                position[0], position[1], position[2],
+                symbol, charge, 0.0,
+                forces[0], forces[1], forces[2])]
+                for position, symbol, charge, forces in zip(
+                    (np.array(row.positions) * ANG2BOHR).tolist(),
+                    row.symbols,
+                    row.data.charges,
+                    #  chgarges_list,
+                    (np.array(row.data.forces) * (EV2HARTREE/ANG2BOHR)).tolist())]
+
+            atoms_prep_list += [["energy ",
+                                 row.data.energy * EV2HARTREE], ["charge 0.0"], ["end"]]
+
+            for line in atoms_prep_list:
                 for item in line:
                     fl.write(str(item))
                 fl.write("\n")
-                fl.flush()
-            energies.append(result[1])
+    fl.close()
+
+parser = argparse.ArgumentParser(description="Give something ...")
+parser.add_argument("-db", type=str, required=True)
+parser.add_argument("-N", type=int, default=0, required=False)
+args = parser.parse_args()
 
 
-def calc_cohesive_E(row, total_E, free_atoms_energies):
-    chemical_symbols = row.symbols
-    chemical_symbols_numbers = {i:chemical_symbols.count(i) for i in chemical_symbols}
-    free_energies_all_atoms = 0.0
-    for chemical_symbol, number_of_atoms in chemical_symbols_numbers.items():
-        free_energies_all_atoms += number_of_atoms * free_atoms_energies[chemical_symbol]
-    return (total_E - free_energies_all_atoms) / row.natoms
+db = connect(args.db)
+N = args.N
 
-#  db_path = "../../geom_files/"\
-#      + "nonEquGeometriesEnergyForcesWithORCA_TZVP_fromScaling_IRMOFseries1_4_6_7_10_merged_50000_ev.db"
-#      #  + "nonEquGeometriesEnergyForcesWithORCA_TZVP_fromScaling.db" # for  just MOF5
-db_path = "../../../deepMOF/HDNNP/prepare_data/workingOnDataBase/"\
-    + "nonEquGeometriesEnergyForcesWithORCA_TZVP_fromScaling_IRMOFseries1_4_6_7_10_merged_50000_ev.db"
-db = connect(db_path)
-n_db = len(db)
+if N == 0:
+    aseDb2Runner(db)
+else:
+    rndAseDb2Runner(db)
 
-#  conf_type = "mof5_f1"
-conf_type = False
-#  conf_type = "irmofseries7_f1"
-N = 1000
-
-# if you set N >0 which is number of data point as intger
-# will execute rondum selection
-fl_name = "input.data"
-names = []
-energies = []
-with open(fl_name, "w") as fl:
-    with Pool(processes=56) as pool:
-        if N != 0:
-            while len(energies) < N:
-                iterable = random.sample(range(1, n_db+1), N)
-                run_func()
-        else:
-            iterable = range(1, n_db+1)
-            run_func()
-
-df = pd.DataFrame()
-df["Energy(eV/atom)"] = energies
-df.to_csv("cohsive_energies.csv", index=False)
