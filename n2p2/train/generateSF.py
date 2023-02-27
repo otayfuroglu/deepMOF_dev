262d7f5534b6310c027412a53832b99e9fb27dd7
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-02-27 23:46:39 +0100

    claarified

--- n2p2/train/generateSF.py
+++ n2p2/train/generateSF.py
@@ -2,42 +2,36 @@
 import sys
 import getpass
 USER = getpass.getuser()
-BASE_DIR = f"/truba_scratch/{USER}/deepMOF_dev"
+BASE_DIR = f"/kernph/{USER}/deepMOF_dev"
 sys.path.append(f"{BASE_DIR}/n2p2/python/symfunc_paramgen/src")
 from sfparamgen import SymFuncParamGenerator
 
 
 
-
-myGenerator = SymFuncParamGenerator( elements=['H', 'C', 'O', 'Zn'],
+myGenerator = SymFuncParamGenerator( elements=['H', 'Li', 'Al'],
                                     r_cutoff = 6.0)
 
-
-#  myGenerator.symfunc_type = 'weighted_radial'
-#  myGenerator.generate_radial_params(rule='gastegger2018',
 myGenerator.symfunc_type = 'radial'
 myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='shift', nb_param_pairs=20, r_lower=1.3)
+                                   mode='center', nb_param_pairs=6)
 
 f = open('sym_func_params.txt', 'w')
 myGenerator.write_settings_overview(fileobj=f)
 myGenerator.write_parameter_strings(fileobj=f)
 
-zetas = [1]
+zetas = [1.0, 2.0, 4.0, 16.0]
 myGenerator.zetas = zetas
-#  myGenerator.symfunc_type = 'weighted_angular'
-#  myGenerator.generate_radial_params(rule='gastegger2018',
 myGenerator.symfunc_type = 'angular_narrow'
 myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='center', nb_param_pairs=3, r_lower=1.3)
+                                   mode='center', nb_param_pairs=3)
 myGenerator.write_settings_overview(fileobj=f)
 myGenerator.write_parameter_strings(fileobj=f)
 
-myGenerator.symfunc_type = 'angular_wide'
-myGenerator.generate_radial_params(rule='imbalzano2018',
-                                   mode='center', nb_param_pairs=3, r_lower=1.3)
-myGenerator.write_settings_overview(fileobj=f)
-myGenerator.write_parameter_strings(fileobj=f)
-
+#  myGenerator.symfunc_type = 'angular_wide'
+#  myGenerator.generate_radial_params(rule='imbalzano2018',
+#                                     mode='center', nb_param_pairs=3)
+#  myGenerator.write_settings_overview(fileobj=f)
+#  myGenerator.write_parameter_strings(fileobj=f)
+#
 
 
