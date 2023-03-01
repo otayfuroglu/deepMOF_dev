0fb1f1477103b7723903eada630ff2fafbfd70aa
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-03-01 17:44:50 +0100

    black box

--- n2p2/prediction/runCalcWithN2p2.py
+++ n2p2/prediction/runCalcWithN2p2.py
@@ -5,64 +5,16 @@ from __future__ import print_function
 import pynnp
 import numpy as np
 from ase.db import connect
-import os, sys
-import torch
+from ase import Atoms
 
+import os, shutil
+import torch
+import tqdm
 
 from n2p2AseInterFace import n2p2Calculator
 import argparse
 
-parser = argparse.ArgumentParser(description="Give something ...")
-#  parser.add_argument("-mof_num", "--mof_num",
-#                      type=int, required=True,
-                    #  help="..")
-parser.add_argument("-val_type", "--val_type",
-                    type=str, required=True,
-                    help="..")
-parser.add_argument("-data_path", "--data_path",
-                    type=str, required=True,
-                    help="..")
-parser.add_argument("-MODEL_DIR", "--MODEL_DIR",
-                    type=str, required=True,
-                    help="..")
-parser.add_argument("-RESULT_DIR", "--RESULT_DIR",
-                    type=str, required=True,
-                    help="..")
-parser.add_argument("-best_epoch", "--best_epoch",
-                    type=int, required=True,
-                    help="..")
-args = parser.parse_args()
-
-
-def aseDB2n2p2(row):
-    fl = open("input.data", "w")
-    atoms_prep_list = [["begin"], ["comment ", row.name]]
-    atom_template = 'atom {:10.6f} {:10.6f} {:10.6f} {:2s} {:10.6f} {:10.6f} {:10.6f} {:10.6f} {:10.6f}'
-    atoms_prep_list += [[atom_template.format(
-        position[0], position[1], position[2],
-        symbol, 0.0, 0.0,
-        forces[0], forces[1], forces[2])]
-        for position, symbol, forces in zip(
-            row.positions.tolist(), row.symbols, row.forces.tolist())]
-    atoms_prep_list += [["energy ", row.energy], ["charge 0.0"], ["end"]]
-    for line in atoms_prep_list:
-        for item in line:
-            fl.write(str(item))
-        fl.write("\n")
-    fl.close()
-
-
-def prepareCalc(best_epoch):
-
-    os.system(f"cp {MODEL_DIR}/input.nn {RESULT_DIR}/")
-    os.system(f"cp {MODEL_DIR}/scaling.data {RESULT_DIR}/")
-
-    weights_files = [item for item in os.listdir(MODEL_DIR) if "weights" in item]
-    best_weights_files = [item for item in weights_files if int(item.split(".")[-2]) == best_epoch]
-    assert len(best_weights_files) != 0, "Erro: NOT FOUND best epoch number"
-
-    for best_weights_file in best_weights_files:
-        os.system(f"cp {MODEL_DIR}/{best_weights_file} {RESULT_DIR}/{best_weights_file[:11]}.data")
+from multiprocessing import Pool, current_process
 
 
 def get_fmax_idx(forces):
@@ -99,103 +51,108 @@ def get_fmax_componentFrom_idx(forces, fmax_component_idx):
     return forces[fmax_component_idx[0], fmax_component_idx[1]].item()
 
 
-def runPredict(row, fl_obj):
-    aseDB2n2p2(row)
-    # Initialize NNP prediction mode.
-    p = pynnp.Prediction()
-    # Read settings and setup NNP.
-    p.setup()
-    # Read in structure.
-    p.readStructureFromFile()
-    # Predict energies and forces.
-    p.predict()
-    # Shortcut for structure container.
-    s = p.structure
+def runPredict(idx):
 
-    natoms = len(row.symbols)
-    n2p2_forces = np.zeros([natoms,3])
-    for i, atom in enumerate(s.atoms):
-        n2p2_forces[i, :] = atom.f.r
+    current = current_process()
+    proc_dir = RESULT_DIR + "/tmp_%s" % current._identity
+    if not os.path.exists(proc_dir):
+        os.mkdir(proc_dir)
+    os.chdir(proc_dir)
 
-    # first get fmax indices than get qm_fmax and n2p2 fmax component
-    qm_fmax_component_idx = get_fmax_idx(row.forces)
-    qm_fmax_component = get_fmax_componentFrom_idx(row.forces,
-                                                   qm_fmax_component_idx)
-    n2p2_fmax_component = get_fmax_componentFrom_idx(n2p2_forces, qm_fmax_component_idx)
+    calculator = n2p2Calculator(model_dir=MODEL_DIR,
+                                best_epoch=args.best_epoch,
+                                energy_units=args.energy_u,
+                                length_units=args.length_u,
+                               )
+    row = db.get(idx+1)
 
-    diffE = s.energyRef - s.energy
-    diffEpa = diffE / natoms
-
-    diffFamxComp = n2p2_fmax_component - qm_fmax_component
-    print("{},{},{},{}".format(row.name, diffE, diffEpa, diffFamxComp), file=fl_obj)
-   #   print("------------")
-   #   print("Structure 1:")
-   #   print("------------")
-   #   print("numAtoms           : ", s.numAtoms)
-   #   print("numAtomsPerElement : ", s.numAtomsPerElement)
-   #   print("------------")
-   #   print("Energy (Ref) : ", s.energyRef)
-   #   print("Energy (NNP) : ", s.energy)
-   #   print("------------")
-   #   for atom in s.atoms:
-   #       print(atom.index, atom.element, p.elementMap[atom.element], atom.f.r)
-
-def runPredict_test(row, calculator, fl_obj):
-
-    atoms = row.toatoms()
+    #  atoms = row.toatoms()
+    atoms = Atoms(symbols=row.symbols, positions=row.positions, cell=row.cell)
+    atoms.pbc = True
     atoms.set_calculator(calculator)
 
-    qm_energy = row.energy
+    qm_energy = row.data.energy
+    qm_forces = np.array(row.data.forces)
     # first get fmax indices than get qm_fmax and n2p2 fmax component
-    qm_fmax_component_idx = get_fmax_idx(row.forces)
-    qm_fmax_component = get_fmax_componentFrom_idx(row.forces,
+    qm_fmax_component_idx = get_fmax_idx(qm_forces)
+    qm_fmax_component = get_fmax_componentFrom_idx(qm_forces,
                                                    qm_fmax_component_idx)
 
+    #  n2p2_energy = atoms.get_potential_energy()
     n2p2_energy = atoms.get_potential_energy()
     n2p2_forces = atoms.get_forces()
+    #  print(n2p2_energy, qm_energy)
     n2p2_fmax_component = get_fmax_componentFrom_idx(n2p2_forces, qm_fmax_component_idx)
 
-    diffE = qm_energy - n2p2_energy
-    diffEpa = diffE / len(atoms)
+    return idx, len(atoms), qm_energy,  n2p2_energy, qm_fmax_component, n2p2_fmax_component
 
-    diffFamxComp = n2p2_fmax_component - qm_fmax_component
-    print("{},{},{},{}".format(row.name, diffE, diffEpa, diffFamxComp), file=fl_obj)
-   #   print("------------")
-   #   print("Structure 1:")
-   #   print("------------")
-   #   print("numAtoms           : ", s.numAtoms)
-   #   print("numAtomsPerElement : ", s.numAtomsPerElement)
-   #   print("------------")
-   #   print("Energy (Ref) : ", s.energyRef)
-   #   print("Energy (NNP) : ", s.energy)
-   #   print("------------")
-   #   for atom in s.atoms:
-   #       print(atom.index, atom.element, p.elementMap[atom.element], atom.f.r)
 
-mode = args.val_type
+parser = argparse.ArgumentParser(description="Give something ...")
+parser.add_argument("-data_path", "--data_path",
+                    type=str, required=True,
+                    help="..")
+parser.add_argument("-energy_u", "--energy_u",
+                    type=str, required=True,
+                    help="..")
+parser.add_argument("-length_u", "--length_u",
+                    type=str, required=True,
+                    help="..")
+parser.add_argument("-MODEL_DIR", "--MODEL_DIR",
+                    type=str, required=True,
+                    help="..")
+parser.add_argument("-RESULT_DIR", "--RESULT_DIR",
+                    type=str, required=True,
+                    help="..")
+parser.add_argument("-best_epoch", "--best_epoch",
+                    type=int, required=True,
+                    help="..")
+parser.add_argument("-nproc", "--nproc",
+                    type=int, required=True,
+                    help="..")
+args = parser.parse_args()
+
 MODEL_DIR = args.MODEL_DIR
 RESULT_DIR = args.RESULT_DIR
 data_path = args.data_path
 
 
-def main():
+#  os.chdir(RESULT_DIR)
+db = connect(data_path)
+len_db = db.count()
+#  db = db.select()
 
-    #  prepareCalc(best_epoch=57)
-    #  os.chdir(RESULT_DIR)
-    #  db_path = "../../../../deepMOF/HDNNP/prepare_data/workingOnDataBase/"\
-    #      + "nonEquGeometriesEnergyForcesWithORCAFromMD_testSet.db"
-        #  + "nonEquGeometriesEnergyForcesWithORCA_TZVP_fromScaling.db" # for  just MOF5
-        #  + "nonEquGeometriesEnergyForcesWithORCA_TZVP_fromScaling_IRMOFseries1_4_6_7_10_merged_50000_ev.db"
-    os.chdir(RESULT_DIR)
-    calculator = n2p2Calculator(model_dir=MODEL_DIR, best_epoch=args.best_epoch)
-    db = connect(data_path)
-    db = db.select()
-    fl_obj = open("results.csv", "w")
-    fl_obj.write("Name,DifEnergy(eV),DifEnergyPa(eV),diffFmaxComp(eV/A)\n")
-    for i, row in enumerate(db):
-        runPredict_test(row, calculator, fl_obj)
-        if i == 30:
-            break
-    fl_obj.close()
 
-main()
+fl_energy = open(f"{RESULT_DIR}/energy.csv", "w")
+fl_energy.write("Name,qmEnergyPa(eV),modelEnergyPa(eV)\n")
+
+fl_forces = open(f"{RESULT_DIR}/forces.csv", "w")
+fl_forces.write("Name,qmMaxForce(eV),modelMaxForce(eV)\n")
+
+fl_summary = open(f"{RESULT_DIR}/diff.csv", "w")
+fl_summary.write("Name,DifEnergy(eV),DifEnergyPa(eV),diffFmaxComp(eV/A)\n")
+
+#  for idx in range(len_db):
+    #  runPredict_test(idx)
+    #  quit()
+
+pool = Pool(processes=args.nproc)
+result_list_tqdm = []
+# implementation of  multiprocessor in tqdm. Ref.https://leimao.github.io/blog/Python-tqdm-Multiprocessing/
+for result in tqdm.tqdm(pool.imap_unordered(func=runPredict, iterable=range(len_db)), total=len_db):
+    result_list_tqdm.append(result)
+    idx, n_atoms, qm_energy,  n2p2_energy, qm_fmax_component, n2p2_fmax_component = result
+    diffE = qm_energy - n2p2_energy
+    diffEpa = diffE / n_atoms
+
+    diffFamxComp = n2p2_fmax_component - qm_fmax_component
+    print("Frame_{},{},{}".format(idx, qm_energy/n_atoms , n2p2_energy/n_atoms), file=fl_energy)
+    print("Frame_{},{},{}".format(idx, qm_fmax_component, n2p2_fmax_component), file=fl_forces)
+    print("Frame_{},{},{},{}".format(idx, diffE, diffEpa, diffFamxComp), file=fl_summary)
+
+fl_energy.close()
+fl_forces.close()
+fl_summary.close()
+
+# remove temporary directories
+for tmp_dir in [tmp_dir for tmp_dir in os.listdir(RESULT_DIR) if "tmp_" in tmp_dir]:
+    shutil.rmtree(f"{RESULT_DIR}/{tmp_dir}")
