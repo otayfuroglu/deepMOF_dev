40e4facd43ac3cc4183a9a8c9204a9743b086110
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-04-09 04:20:38 +0200

    black

--- resultAnalysis/getPoseByCoordNumAndOptimize.py
+++ resultAnalysis/getPoseByCoordNumAndOptimize.py
@@ -7,18 +7,56 @@ from ase.neighborlist import (NeighborList,
                               NewPrimitiveNeighborList,
                               PrimitiveNeighborList,
                               natural_cutoffs)
+from ase.calculators.vasp import Vasp
 from pymatgen.core import Structure
 from pymatgen.analysis.local_env import  (BrunnerNN_reciprocal,
                                           BrunnerNN_real,
                                           CrystalNN,
                                           MinimumDistanceNN)
 
+from pathlib import Path
 import os
 import tqdm
 import numpy as np
+import pandas as pd
 
 from numba import jit, float32
 import argparse
+
+
+
+def setVaspCalculator(calc):
+    calc.set(
+     system = f"self_energy_R2SCAN",
+     prec = 'Accurate',
+     ismear = 3, # gaussian smearing
+     sigma = 0.02,
+     isym = -1,
+     #  isym = 2,
+     symprec = 1.0e-06, # to overcome Inconsistent Bravais lattice types found for crystalline error
+     nelm = 60, # max number of scf iterations
+     lreal = "AUTO", # recommended by vasp for more than 30 atoms
+     isif = 3, # calculate stress and optimize cell shape and volume
+     ibrion = 2, #2 # use 2 for conj. grad.
+     enaug = 800, # test using the default here!                    <--------
+     addgrid = True, # reduces force noise, but vasp says do not this as default in all computations? <--------
+     algo = "Normal", # recommended for GPU (maybe also try Fast ?)                                   <--------
+     metagga = "R2SCAN",
+     lasph = True, # recommended for metaggas in the vasp wiki, increases accuracy
+     kpar = 1, # set to numbers of gpus
+     ncore = 4, # for gpu
+     nsim = 1, # ###### 1 was used for many calculations
+     ediff = 1.0e-07, # changed from 1.0e-07
+     ediffg = -0.005,
+     encut = 800,
+     #  encut = 500, # for system which contain Ca or K elements
+     nsw = nsw, #0 #600 # number of optimization steps
+     kspacing = 0.2,
+     kgamma   = True, # from emergence paper (should converge faster)
+     pstress = 0, # pressure in MD
+     lwave = False, # dont save wavefunction for single point run
+     lcharg = False, # same for charge
+    )
 
 
 
@@ -46,122 +84,118 @@ def getCellVolume():
         #  break
 
 
-@jit(float32(float32, float32, float32))
-def f_cut(d_kl, Tx, Vx):
-    #  print(d_kl, Tx, Vx)
-
-    K = (d_kl - Tx) / (Vx - Tx)
-    f = np.array((2 * K + 1) * ((K - 1) ** 2))
-    f[d_kl<Tx] = 1
-    f[Vx<d_kl] = 0
-    #  print(f)
-    return f
-
-
-def get_1NN2NN_distances(nn, struc, center_atom_i):
-    nn_distances = []
-    site_idexes = []
-    for shel_i in [1, 2]:
-        distances = []
-        for site in nn.get_nn_shell_info(struc, center_atom_i, shel_i):
-            #  print(site)
-            site_i = site["site_index"]
-            if not site_i in site_idexes:
-                site_idexes.append(site_i)
-                distances += [struc.get_distance(center_atom_i, site_i)]
-        #  if len(distances) == 0:
-            #  distances.append(0)
-        try:
-            nn_distances.append(min(distances))
-        except:
-            return 0, 0.0
-    return site_idexes, nn_distances
-
-
-def get_coord_num(nn, struc, center_atom_i):
-    coord_num = 0
-    site_idexes, nn_distances = get_1NN2NN_distances(nn, struc, center_atom_i)
-    if nn_distances == 0.0:
-        return None
-
-    passed_site_idexes = []# to get under control duplication and self correlation
-    for shel_i in [1, 2]:
-        for site_i in site_idexes:
-            #  print(site_i)
-            if not site_i in passed_site_idexes: # to get under control duplication and self correlation
-                passed_site_idexes.append(site_i)
-                distance = struc.get_distance(center_atom_i, site_i)
-                coord_num += f_cut(d_kl=distance, Tx=nn_distances[0], Vx=nn_distances[1])
-    return coord_num
-
-
-def run_get_coord_num():
-    nn = CrystalNN(search_cutoff=12)
-    center_atom_i = 2 # for Al atom index
-    #  Al_index = [atom.index for atom in atoms if atom.symbol == "Al"]
-    #  center_atom_i = Al_index[0]
-    fl = open(f"coord_nums.csv", "w")
-    for root, dirs, files in os.walk("./vasp_opt_geoms"):
-        for file_name in files:
-            print(file_name)
-            struc = Structure.from_file(f"{root}/{file_name}") #read in CIF as Pymatgen Structure
-            coord_num = get_coord_num(nn, struc, center_atom_i)
-            if coord_num == 1:
-                struc.make_supercell([2, 2, 2])
-                coord_num = get_coord_num(nn, struc, center_atom_i)
-            print(f"{file_name},{coord_num}", file=fl)
-
 
 def lammps2AseAtoms(lammps_atoms, atom_type_symbol_pair):
     symbols = [atom_type_symbol_pair[key] for key in lammps_atoms.get_atomic_numbers()]
     return Atoms(symbols=symbols, positions=lammps_atoms.positions, cell=lammps_atoms.cell)
 
 
-def get_pose():
-    interval = 1000
-    idxes = slice(0, -1, interval)
-    atom_type_symbol_pair = {1:"Al", 2:"Li", 3:"H"}
-    lammps_trj_path = "../scicore/nnp_train_on26kdata_npt_02timestep_500K_v2_triTdamp100Pdamp100/alanates_1Bar_500K.lammpstrj"
-    lammps_trj = read(lammps_trj_path, format="lammps-dump-text", index=idxes, parallel=True)
+parser = argparse.ArgumentParser(description="Give something ...")
+parser.add_argument("-trj_path", type=str, required=True, help="..")
+parser.add_argument("-csv_path", type=str, required=True, help="..")
+parser.add_argument("-coord_type", type=str, required=True, help="..")
+parser.add_argument("-IDX", type=int, required=True, default=1, help="..")
+args = parser.parse_args()
 
-    nn = CrystalNN(search_cutoff=12)
-    center_atom_i = 2 # for Al atom index
-    k, l = 0, 0
-    fl = open(f"coord_nums.csv", "w")
-    opt_fl = open(f"opt_coord_nums.csv", "w")
-    for i, lammps_atoms in enumerate(lammps_trj):
-        atoms = lammps2AseAtoms(lammps_atoms, atom_type_symbol_pair)
-        #  Al_index = [atom.index for atom in atoms if atom.symbol == "Al"]
+df = pd.read_csv(args.csv_path)
+
+coord_type = args.coord_type
+if coord_type == "polymeric":
+    df = df.loc[df["CoordNum"] > 5.9].reset_index()
+    shift = int(len(df)/20) # for long interval selection
+    idx = int(df["Frames"][(shift -1)* args.IDX].split("_")[-1])
+    coord_num = df["CoordNum"][(shift -1)* args.IDX]
+elif coord_type == "isolated":
+    df = df.loc[df["CoordNum"] < 4.4].reset_index()
+    shift = int(len(df)/20) # for long interval selection
+    idx = int(df["Frames"][(shift -1)* args.IDX].split("_")[-1])
+    coord_num = df["CoordNum"][(shift -1)* args.IDX]
+else:
+    print("Enter coord_type correctly")
+    quit(1)
+
+atom_type_symbol_pair = {1:"Al", 2:"Li", 3:"H"}
+#  lammps_trj_path = "../scicore/nnp_train_on26kdata_npt_02timestep_500K_v2_triTdamp100Pdamp100/alanates_1Bar_500K.lammpstrj"
+lammps_trj = read(args.trj_path, format="lammps-dump-text", index=":", parallel=True)
+
+fl = open(f"{coord_type}_coord_nums.csv", "a")
+print(f"{coord_type}_{idx},{coord_num}", file=fl)
+
+opt_fl = open(f"opt_{coord_type}_coord_nums.csv", "a")
+selected_dir = "selectedByCoordNum"
+Path(f"{selected_dir}").mkdir(parents=True, exist_ok=True)
+Path(f"vasp_calc/{idx}").mkdir(parents=True, exist_ok=True)
+
+cwd = os.getcwd()
+
+# set VASP calc objects
+calc = Vasp()
+nsw = 100
+setVaspCalculator(calc)
+
+lammps_atoms = lammps_trj[idx]
+atoms = lammps2AseAtoms(lammps_atoms, atom_type_symbol_pair)
+write(f"./selectedByCoordNum/{coord_type}_{idx}.cif", atoms)
+
+os.chdir(f"vasp_calc/{idx}")
+atoms.pbc = [1, 1, 1]
+atoms.calc = calc
+atoms.get_potential_energy()
+
+from mdAnalysisLammpsTrjCalcCoordNum import get_coord_num
+
+nn = CrystalNN(search_cutoff=12)
+
+Al_index = [atom.index for atom in atoms if atom.symbol == "Al"]
+center_atom_i = Al_index[0]
+
+opt_coord_num = get_coord_num(nn, atoms, center_atom_i)
+os.chdir(cwd)
+
+write(f"./selectedByCoordNum/opt_{coord_type}_{idx}.cif", atoms)
+print(f"opt_{coord_type}_{idx},{opt_coord_num}", file=opt_fl)
 
 
-        fl_name = "test"
-        write(f"{fl_name}.cif", atoms)
-        struc = Structure.from_file(f"{fl_name}.cif") #read in CIF as Pymatgen Structure
-        #  struc.make_supercell([2, 2, 2])
-        coord_num = get_coord_num(nn, struc, center_atom_i)
-        print(coord_num)
-
-        if k <= 20 and coord_num > 5.3:
-            print(f"polymeric_{i},{coord_num}", file=fl)
-            #  write(f"./selectedByCoordNum/polymeric_{i}.cif", atoms)
-            #NOTE add vasp opt
-            print(f"opt_polymeric_{i},{coord_num}", file=opt_fl)
-            k += 1
-        elif l <= 20 and coord_num < 4.5:
-            print(f"isolated_{i},{coord_num}", file=fl)
-            #  write(f"./selectedByCoordNum/isolated_{i}.cif", atoms)
-            #NOTE add vasp opt
-            print(f"opt_isolated_{i},{coord_num}", file=opt_fl)
-            l += 1
-
-
-#  parser = argparse.ArgumentParser(description="Give something ...")
-#  parser.add_argument("-trj_path", type=str, required=True, help="..")
-#  parser.add_argument("-interval", type=int, required=False, default=1, help="..")
-#  args = parser.parse_args()
+#  for i, lammps_atoms in enumerate(lammps_trj):
+#      atoms = lammps2AseAtoms(lammps_atoms, atom_type_symbol_pair)
+#      #  Al_index = [atom.index for atom in atoms if atom.symbol == "Al"]
 #
-#  lammps_trj_path = args.trj_path
-#  interval = args.interval
-get_pose()
+#
+#      #  fl_name = "test"
+#      #  write(f"{fl_name}.cif", atoms)
+#      #  struc = Structure.from_file(f"{fl_name}.cif") #read in CIF as Pymatgen Structure
+#      #  struc.make_supercell([2, 2, 2])
+#      coord_num = get_coord_num(nn, atoms, center_atom_i)
+#      #  print(coord_num)
+#
+#      if k <= 20 and coord_num > 5.4:
+#          print(f"polymeric_{i},{coord_num}", file=fl)
+#          write(f"./selectedByCoordNum/polymeric_{i}.cif", atoms)
+#
+#          os.chdir("vasp_calc")
+#          atoms.pbc = [1, 1, 1]
+#          atoms.calc = calc
+#          atoms.get_potential_energy()
+#          os.chdir(cwd)
+#
+#          coord_num = get_coord_num(nn, atoms, center_atom_i)
+#          print(f"opt_polymeric_{i},{coord_num}", file=opt_fl)
+#          k += 1
+#      elif l <= 20 and coord_num < 4.5:
+#          print(f"isolated_{i},{coord_num}", file=fl)
+#          write(f"./selectedByCoordNum/isolated_{i}.cif", atoms)
+#
+#          os.chdir("vasp_calc")
+#          atoms.pbc = [1, 1, 1]
+#          atoms.calc = calc
+#          atoms.get_potential_energy()
+#          os.chdir(cwd)
+#
+#          coord_num = get_coord_num(nn, atoms, center_atom_i)
+#          print(f"opt_isolated_{i},{coord_num}", file=opt_fl)
+#          l += 1
+#
+#      if k == 20 and l == 20:
+#          break
 
 
