a81fb8823dfef930420b1626e10b20b3adc242f3
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-04-09 04:20:12 +0200

    some changes

--- resultAnalysis/mdAnalysisLammpsTrjCalcCoordNum.py
+++ resultAnalysis/mdAnalysisLammpsTrjCalcCoordNum.py
@@ -57,7 +57,13 @@ def get_1NN2NN_distances(nn, struc, center_atom_i):
     return site_idexes, nn_distances
 
 
-def get_coord_num(nn, struc, center_atom_i):
+def get_coord_num(nn, atoms, center_atom_i):
+
+    fl_name = "tmp"
+    write(f"{fl_name}.cif", atoms)
+    struc = Structure.from_file(f"{fl_name}.cif") #read in CIF as Pymatgen Structure
+    #  struc.make_supercell([2, 2, 2])
+
     coord_num = 0
     site_idexes, nn_distances = get_1NN2NN_distances(nn, struc, center_atom_i)
     if nn_distances == 0.0:
@@ -120,14 +126,9 @@ def task(idx):
     center_atom_i = Al_index[0]
 
 
-    fl_name = "test"
-    write(f"{fl_name}.cif", atoms)
-    site_idx = 0 #index of atom to get coordination environment
-    struc = Structure.from_file(f"{fl_name}.cif") #read in CIF as Pymatgen Structure
-    #  struc.make_supercell([2, 2, 2])
-    coord_num = get_coord_num(nn, struc, center_atom_i)
+    coord_num = get_coord_num(nn, atoms, center_atom_i)
     os.chdir(cwd)
-    return coord_num
+    return f"frame_{idx}", coord_num
     #  print(coord_num)
 
     #  if coord_num == 1:
@@ -143,37 +144,45 @@ def task(idx):
 
     #  struc = Structure(lattice=atoms.cell, species=atoms.get_chemical_symbols(), coords=atoms.get_positions())
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Give something ...")
+    parser.add_argument("-trj_path", type=str, required=True, help="..")
+    parser.add_argument("-interval", type=int, required=False, default=1, help="..")
+    parser.add_argument("-nproc", type=int, required=True, help="..")
+    args = parser.parse_args()
+    lammps_trj_path = args.trj_path
+    #  nn = MinimumDistanceNN()
+    #  nn = BrunnerNN_real(cutoff=12)
+    #  nn = BrunnerNN_reciprocal()
+    #  nn = CrystalNN(search_cutoff=12)
 
-parser = argparse.ArgumentParser(description="Give something ...")
-parser.add_argument("-trj_path", type=str, required=True, help="..")
-parser.add_argument("-interval", type=int, required=False, default=1, help="..")
-parser.add_argument("-nproc", type=int, required=True, help="..")
-args = parser.parse_args()
-lammps_trj_path = args.trj_path
-#  nn = MinimumDistanceNN()
-#  nn = BrunnerNN_real(cutoff=12)
-#  nn = BrunnerNN_reciprocal()
-#  nn = CrystalNN(search_cutoff=12)
+    nn = CrystalNN(search_cutoff=12)
+    idxes = slice(0, -1, args.interval)
+    atom_type_symbol_pair = {1:"Al", 2:"Li", 3:"H"}
+    #  lammps_trj_path = "../alanates/cscs/nnp_train_on16kdata_nvt_02timestep_1500K_2ns/alanates_1Bar_1500K.lammpstrj"
+    lammps_trj = read(lammps_trj_path, format="lammps-dump-text", index=idxes, parallel=True)
+    #  db_path = f"{lammps_trj_path.split('/')[-1].replace('.lammpstrj', '')}.db"
+    #  if not os.path.exists(db_path):
+    #      lammps_trj = read(lammps_trj_path, format="lammps-dump-text", index=idxes, parallel=True)
+    #      lammsTrj2AseDb(lammps_trj, db_path)
 
-nn = CrystalNN(search_cutoff=12)
-coord_nums = []
-idxes = slice(0, -1, args.interval)
-atom_type_symbol_pair = {1:"Al", 2:"Li", 3:"H"}
-#  lammps_trj_path = "../alanates/cscs/nnp_train_on16kdata_nvt_02timestep_1500K_2ns/alanates_1Bar_1500K.lammpstrj"
-lammps_trj = read(lammps_trj_path, format="lammps-dump-text", index=idxes, parallel=True)
-#  db_path = f"{lammps_trj_path.split('/')[-1].replace('.lammpstrj', '')}.db"
-#  if not os.path.exists(db_path):
-#      lammps_trj = read(lammps_trj_path, format="lammps-dump-text", index=idxes, parallel=True)
-#      lammsTrj2AseDb(lammps_trj, db_path)
+    #  db = connect(db_path)
+    coord_nums = []
+    frames = []
+    len_trj = len(lammps_trj)
+    with Pool(args.nproc) as pool:
+        # implementation of  multiprocessor in tqdm. Ref.https://leimao.github.io/blog/Python-tqdm-Multiprocessing/
+        for result in tqdm.tqdm(pool.imap_unordered(func=task, iterable=range(len_trj)), total=len_trj):
+            if result:
+                frames.append(result[0])
+                coord_nums.append(result[1])
 
-#  db = connect(db_path)
-len_trj = len(lammps_trj)
-with Pool(args.nproc) as pool:
-    # implementation of  multiprocessor in tqdm. Ref.https://leimao.github.io/blog/Python-tqdm-Multiprocessing/
-    for result in tqdm.tqdm(pool.imap_unordered(func=task, iterable=range(len_trj)), total=len_trj):
-        if result:
-            coord_nums.append(result)
+    import pandas as pd
+    df = pd.DataFrame()
+    df["Frames"] = frames
+    df["CoordNum"] = coord_nums
+    df.to_csv("coord_nums.csv")
 
-import matplotlib.pyplot as plt
-plt.plot(range(len(coord_nums)), np.array(coord_nums))
-plt.savefig("coord_num.png")
+    #  import matplotlib.pyplot as plt
+    #  plt.plot(range(len(coord_nums)), np.array(coord_nums))
+    #  plt.savefig("coord_num_v0.png")
