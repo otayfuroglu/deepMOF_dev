66ea58f43f0709002995bacaa56dcd779663be90
Omer Tayfuroglu <tayfur0000@login01.cluster.bc2.ch>
2023-10-19 16:31:53 +0200

    some changes

--- n2p2/prediction/runCalcWithN2p2.py
+++ n2p2/prediction/runCalcWithN2p2.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import
 from __future__ import print_function
 import pynnp
 import numpy as np
-from ase.db import connect
+#  from ase.db import connect
+from ase.io import read
 from ase import Atoms
 
 import os, shutil
@@ -53,38 +54,40 @@ def get_fmax_componentFrom_idx(forces, fmax_component_idx):
 
 def runPredict(idx):
 
-    current = current_process()
-    proc_dir = RESULT_DIR + "/tmp_%s" % current._identity
-    if not os.path.exists(proc_dir):
-        os.mkdir(proc_dir)
-    os.chdir(proc_dir)
+    try:
+        current = current_process()
+        proc_dir = RESULT_DIR + "/tmp_%s" % current._identity
+        if not os.path.exists(proc_dir):
+            os.mkdir(proc_dir)
+        os.chdir(proc_dir)
 
-    calculator = n2p2Calculator(model_dir=MODEL_DIR,
-                                best_epoch=args.best_epoch,
-                                energy_units=args.energy_u,
-                                length_units=args.length_u,
-                               )
-    row = db.get(idx+1)
+        atoms = atoms_list[idx]
+        qm_energy = atoms.get_potential_energy()
+        qm_forces = atoms.get_forces()
+        # first get fmax indices than get qm_fmax and n2p2 fmax component
+        qm_fmax_component_idx = get_fmax_idx(qm_forces)
+        qm_fmax_component = get_fmax_componentFrom_idx(qm_forces,
+                                                       qm_fmax_component_idx)
 
-    #  atoms = row.toatoms()
-    atoms = Atoms(symbols=row.symbols, positions=row.positions, cell=row.cell)
-    atoms.pbc = True
-    atoms.set_calculator(calculator)
+        calculator = n2p2Calculator(model_dir=MODEL_DIR,
+                                    best_epoch=args.best_epoch,
+                                    energy_units=args.energy_u,
+                                    length_units=args.length_u,
+                                   )
+        atoms.pbc = True
+        atoms.set_calculator(calculator)
 
-    qm_energy = row.data.energy
-    qm_forces = np.array(row.data.forces)
-    # first get fmax indices than get qm_fmax and n2p2 fmax component
-    qm_fmax_component_idx = get_fmax_idx(qm_forces)
-    qm_fmax_component = get_fmax_componentFrom_idx(qm_forces,
-                                                   qm_fmax_component_idx)
+        #  n2p2_energy = atoms.get_potential_energy()
+        n2p2_energy = atoms.get_potential_energy()
+        n2p2_forces = atoms.get_forces()
+        #  print(n2p2_energy, qm_energy)
+        n2p2_fmax_component = get_fmax_componentFrom_idx(n2p2_forces, qm_fmax_component_idx)
 
-    #  n2p2_energy = atoms.get_potential_energy()
-    n2p2_energy = atoms.get_potential_energy()
-    n2p2_forces = atoms.get_forces()
-    #  print(n2p2_energy, qm_energy)
-    n2p2_fmax_component = get_fmax_componentFrom_idx(n2p2_forces, qm_fmax_component_idx)
-
-    return idx, len(atoms), qm_energy,  n2p2_energy, qm_fmax_component, n2p2_fmax_component
+        return (idx, len(atoms), qm_energy,
+                n2p2_energy, qm_fmax_component,
+                n2p2_fmax_component, qm_forces, n2p2_forces)
+    except:
+        return None
 
 
 parser = argparse.ArgumentParser(description="Give something ...")
@@ -117,19 +120,26 @@ data_path = args.data_path
 
 
 #  os.chdir(RESULT_DIR)
-db = connect(data_path)
-len_db = db.count()
+#  db = connect(data_path)
+#  len_db = db.count()
 #  db = db.select()
 
+atoms_list = read(data_path, index=":")
+len_db = len(atoms_list)
 
-fl_energy = open(f"{RESULT_DIR}/energy.csv", "w")
+keyword = data_path.split("/")[-1].split(".")[0]
+
+fl_energy = open(f"{RESULT_DIR}/{keyword}_energy.csv", "w")
 fl_energy.write("Name,qmEnergyPa(eV),modelEnergyPa(eV)\n")
 
-fl_forces = open(f"{RESULT_DIR}/forces.csv", "w")
-fl_forces.write("Name,qmMaxForce(eV),modelMaxForce(eV)\n")
+fl_max_force = open(f"{RESULT_DIR}/{keyword}_max_force.csv", "w")
+fl_max_force.write("Name,qmMaxForce(eV),modelMaxForce(eV)\n")
 
-fl_summary = open(f"{RESULT_DIR}/diff.csv", "w")
-fl_summary.write("Name,DifEnergy(eV),DifEnergyPa(eV),diffFmaxComp(eV/A)\n")
+fl_forces = open(f"{RESULT_DIR}/{keyword}_forces.csv", "w")
+fl_forces.write("Name,qmForces(eV/A),modelForces(eV/A)\n")
+
+fl_summary = open(f"{RESULT_DIR}/{keyword}_diff.csv", "w")
+fl_summary.write("Name,DifEnergy(eV/A),DifEnergyPa(eV),diffFmaxComp(eV/A)\n")
 
 #  for idx in range(len_db):
     #  runPredict_test(idx)
@@ -139,17 +149,30 @@ pool = Pool(processes=args.nproc)
 result_list_tqdm = []
 # implementation of  multiprocessor in tqdm. Ref.https://leimao.github.io/blog/Python-tqdm-Multiprocessing/
 for result in tqdm.tqdm(pool.imap_unordered(func=runPredict, iterable=range(len_db)), total=len_db):
-    result_list_tqdm.append(result)
-    idx, n_atoms, qm_energy,  n2p2_energy, qm_fmax_component, n2p2_fmax_component = result
-    diffE = qm_energy - n2p2_energy
-    diffEpa = diffE / n_atoms
+    if result:
+        #  result_list_tqdm.append(result)
+        (idx, n_atoms, qm_energy,
+         n2p2_energy, qm_fmax_component,
+         n2p2_fmax_component, qm_forces, n2p2_forces) = result
 
-    diffFamxComp = n2p2_fmax_component - qm_fmax_component
-    print("Frame_{},{},{}".format(idx, qm_energy/n_atoms , n2p2_energy/n_atoms), file=fl_energy)
-    print("Frame_{},{},{}".format(idx, qm_fmax_component, n2p2_fmax_component), file=fl_forces)
-    print("Frame_{},{},{},{}".format(idx, diffE, diffEpa, diffFamxComp), file=fl_summary)
+        diffE = qm_energy - n2p2_energy
+        diffEpa = diffE / n_atoms
+
+        diffFamxComp = n2p2_fmax_component - qm_fmax_component
+        print(idx, ",", qm_energy/n_atoms, ",", n2p2_energy/n_atoms, file=fl_energy)
+        print(idx, ",", qm_fmax_component, ",", n2p2_fmax_component, file=fl_max_force)
+        print(idx, ",", diffE, ",", diffEpa, ",", diffFamxComp, file=fl_summary)
+        for i in range(len(qm_forces.flatten())):
+            print(idx, ",", qm_forces.flatten()[i], ",", n2p2_forces.flatten()[i], file=fl_forces)
+
+
+        fl_energy.flush()
+        fl_max_force.flush()
+        fl_forces.flush()
+        #  fl_summary.flush()
 
 fl_energy.close()
+fl_max_force.close()
 fl_forces.close()
 fl_summary.close()
 
