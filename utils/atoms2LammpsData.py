740f19daa75b8b3b5d43a07d81e9d2ab29462b13
otayfuroglu <https://github.com/otayfuroglu/deepMOF.git>
2024-05-23 18:29:25 +0300

    added specorder

--- utils/atoms2LammpsData.py
+++ utils/atoms2LammpsData.py
@@ -1,9 +1,14 @@
 
 from ase.io import read, write
 from ase.io.lammpsdata import write_lammps_data
+import argparse
 
+parser = argparse.ArgumentParser(description="Give something ...")
+parser.add_argument("-flpath", type=str, required=True, help="..")
+parser.add_argument("-specorder", type=list, nargs='+', required=True, help="..")
+args = parser.parse_args()
 
-flpath = "./mg_mof74.extxyz"
+flpath = args.flpath
 atoms = read(flpath)
 #  atoms.center(vacuum=5.0)
 #  write("./MgF1.extxyz", atoms)
