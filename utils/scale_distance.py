6cccf45748008228d1522e171d895243ec8ac141
otayfuroglu <tayfuroglu@gmail.com>
2025-09-09 15:12:50 +0300

    small

--- utils/scale_distance.py
+++ utils/scale_distance.py
@@ -1,14 +1,34 @@
 
 
 from ase.io import read, write
+import argparse
 
-atoms = read("./opt_MgF2_CO2_1_bonded.xyz")
+
+
+parser = argparse.ArgumentParser(description="Give something ...")
+parser.add_argument("-geom_path", type=str, required=True,)
+args = parser.parse_args()
+
+geom_path = args.geom_path
+base =  geom_path.split("/")[-1].split(".")[0]
+
+atoms = read(geom_path)
+#  atoms.set_distance(126, 544, distance=1.7, indices=[544, 545, 546])
+#  atoms.set_angle(126, 544, 545, angle=145, indices=[544, 545, 546])
+#  write("MgMOF74_clean_fromCORE_addedH_withCO2_final.extxyz", atoms)
+#  quit()
 indices = [atom.index for atom  in atoms]
 #  print(indices)
 
-for distance in range(8, 40):
-    atoms.set_distance(18, 81, distance=distance/10, indices=[81, 82, 83])
-    write("test.extxyz", atoms, append=True)
+for distance in range(10, 71):
+    distance = distance/10
+    print(distance)
+    atoms_scale = atoms.copy()
+    #  atoms_scale.set_distance(18, 81, distance=distance, indices=[81, 82, 83])
+    #  atoms_scale.set_distance(, 83, distance=distance, fix=0, indices=[83, 163, 82])
+    atoms_scale.set_distance(36, 106, distance=distance, fix=0, indices=[106, 107, 108])
+    atoms_scale.info["label"] = f"{base}_scaled_{distance}"
+    write(f"{base}_scaled.extxyz", atoms_scale, append=True)
 
 
 
